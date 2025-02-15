b60acfcf50ec0575a524e9727584a8d78ac7d4bc
vinegraft <982042+vinegraft@users.noreply.github.com>
2025-02-15 17:21:49 -0500

    add framerate cap 60fps

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -16,6 +18,7 @@ def main():
                 return
         screen.fill(0)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
