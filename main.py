91d5e29f9df39156d954691c8957e06bed7285c0
vinegraft <982042+vinegraft@users.noreply.github.com>
2025-02-15 18:30:35 -0500

    add sprite groups for easier rendering

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import pygame
 from constants import *
 from player import Player
 
+updatable = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
+Player.containers = (updatable, drawable)
 
 def main():
     pygame.init()
@@ -15,8 +18,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
